8d7b8a1837988c40f83e9b84837c676fd5003bcd
nathan-roi <nathan.roi@disroot.org>
2026-02-06 16:28:09 +0100

    backend initialize

---
+++ src/main/java/com/nobless/quittances/QuittancesApplication.java
@@ -0,0 +1,13 @@
+package com.nobless.quittances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuittancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuittancesApplication.class, args);
+	}
+
+}
