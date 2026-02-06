085c12555c7bb81f0cc8c0acb5e587fbc27a9b05
nathan-roi <nathan.roi@disroot.org>
2026-02-06 17:16:46 +0100

    creation route api health

---
+++ src/main/java/com/nobless/quittances/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.nobless.quittances.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public Map<String, String> health() {
+        return Map.of("status", "UP", "message", "Le serveur backend tourne !");
+    }
+}
+
